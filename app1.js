6ab9ea490e287ec3fd318b7e5d42052cbca35ba0
Owaise Imdad <owaiseimdad@gmail.com>
2022-08-04 00:17:55 +0530

    Adding the additional code required

--- app1.js
+++ app1.js
@@ -1,10 +1,42 @@
+const { raw } = require("express");
 const request = require("express");
 const express = request();
+const filereader = require("fs");
+
+let rawdata = filereader.readFileSync("countries.json");
+let countries = JSON.parse(rawdata);
+
+// let india = countries["India"];
+// console.log(Object.values(india));
+
 express.get("/", function (req, res) {
   res.send("Hello World");
 });
 
-express.get("/api/compares", function (req, res) {
-  res.send([1, 2, 3, 45, 2]);
+express.get("/api/fact", function (req, res) {
+  res.send(countries);
 });
-express.listen(3000, () => console.log("listening to port 3000..."));
+
+// reading a specific routes for an API.
+express.get("/api/fact/:country/", function (req, res) {
+  //res.send(req.query); // reading query parameters. Query provides additional information to the backend.
+  if (Object.keys(countries).includes(req.params.country)) {
+    var country = countries[req.params.country];
+    res.send(Object.keys(country));
+  } else {
+    res.status(404).send("the given country was not found.");
+  }
+});
+
+express.get("/api/fact/:country/:parameters", function (req, res) {
+  //res.send(req.query); // reading query parameters. Query provides additional information to the backend.
+  if (Object.keys(countries).includes(req.params.country)) {
+    res.send(countries[req.params.country][req.params.parameters]);
+  } else {
+    res.status(404).send("the given parameter was not found");
+  }
+});
+
+// PORT
+const port = process.env.PORT || 3000;
+express.listen(port, () => console.log(`listening to port ${port}...`));
